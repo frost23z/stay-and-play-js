a6537217f2b8c0b33734388c9269e5a8dd6e9a9a
frost23z <80541095+frost23z@users.noreply.github.com>
2026-09-09 13:09:27 +0600

    chore: remove default route for cleaner API structure

--- src/app.js
+++ src/app.js
@@ -8,10 +8,6 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
 app.get('/health', healthRouter)
 
 app.get('/get-property', getPropertyRouter)
